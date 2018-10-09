e86412240f65e70a59cd8e7d01e1a2f31b4064dd
anne-other <anne.c.ince@gmail.com>
2018-10-09 16:10:47 +0100

    Initial Commit

---
+++ src/main/java/com/codeclan/example/folderservice/FolderserviceApplication.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.folderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolderserviceApplication.class, args);
+	}
+}
